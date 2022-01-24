7d771f1f1133c988e41bb68db5f24c50328350e3
shubham7090 <shubham.agg70@gmail.com>
2022-01-24 09:25:06 +0530

    reverse a LinkedList

--- LinkedLists/code.java
+++ LinkedLists/code.java
@@ -15,5 +15,16 @@ public class code{
     public static void main(String[] args){
 
     }
-    
+    //leetcode 206
+    public static Node reverse(Node head){
+        if(head==null||head.next==null)return head;
+        Node prev=null,curr=head;
+        while(curr!=null){
+            Node next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
 }
