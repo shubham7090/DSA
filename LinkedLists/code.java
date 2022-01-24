67d9994bc6ecddd50accf5854bfe0a0dd4287f47
shubham7090 <shubham.agg70@gmail.com>
2022-01-24 09:21:28 +0530

    Layout for LinkedLists Questions

---
+++ LinkedLists/code.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class code{
+    public class Node{
+        int val;
+        Node next;
+        Node(int val,Node next){
+            this.val=val;
+            this.next=next;
+        }
+        Node(int val){
+            this.val=val;
+            this.next=null;
+        }
+    }
+    public static void main(String[] args){
+
+    }
+    
+}
